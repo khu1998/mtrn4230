f1f524b1c01b599c337049c04b9096e61a900cac
Kent Hu <khu.1998@icloud.com>
2020-07-31 13:28:33 +1000

    src/motion.py: now prompts for x,y,z co-ordinates (in global reference frame) then moves manipulator

--- src/motion.py
+++ src/motion.py
@@ -12,44 +12,65 @@ from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
 
 # TODO:
-# put in loop
-# check for valid path
-# communication for position of blocks
+#  - communication block position to pick up
 
 def main():
     moveit_commander.roscpp_initialize(sys.argv)
-    rospy.init_node('move_group_python_interface_tutorial',
-                    anonymous=True)
+    rospy.init_node('motion_planner', anonymous=True)
                     
     robot = moveit_commander.RobotCommander()
-    scene = moveit_commander.PlanningSceneInterface()
+    scene = moveit_commander.PlanningSceneInterface()  # set motion planning scene
+    rospy.sleep(2)  # sleep to load scene otherwise table gets skipped
     group = moveit_commander.MoveGroupCommander("manipulator")
 
-    rospy.sleep(1)
-
-    # table pose relative to frame of robot
+    # add table as collision object
+    # table pose frame is same as robot frame
     table_pose = geometry_msgs.msg.PoseStamped()
     table_pose.header.frame_id = robot.get_planning_frame()
     table_pose.pose.position.x = 0.0
     table_pose.pose.position.y = -0.7
     table_pose.pose.position.z = -0.1
-    table_name = "table"
-    scene.add_box(table_name, table_pose, size=(1, 1, 0.2))
+    scene.add_box("table", table_pose, size=(1, 1, 0.2))
 
     # pose goal is relative to frame of robot
     pose_goal = geometry_msgs.msg.Pose()
-    pose_goal.orientation.w = 1.0
-    pose_goal.position.x = 0.0
-    pose_goal.position.y = -0.7
-    pose_goal.position.z = 0.1
-    group.set_pose_target(pose_goal)
+    pose_goal.orientation.w = 1.0  # hard coded orientation, to be figured out
 
-    plan = group.go(wait=True)
-    # Calling `stop()` ensures that there is no residual movement
-    group.stop()
-    # It is always good to clear your targets after planning with poses.
-    # Note: there is no equivalent function for clear_joint_value_targets()
-    group.clear_pose_targets()
+    while not rospy.is_shutdown():
+        try:
+            position = raw_input("Please enter desired global co-ordinate in the form of <x>,<y>,<z>: ")
+        except EOFError:
+            print() # for new line
+            rospy.loginfo("Got Ctrl+D, stopping motion planning...")
+            break
+
+        rospy.loginfo("Got position -> {}".format(position))
+        try:
+            x, y, z = map(float, position.split(','))
+        except Exception:
+            rospy.loginfo("Failed to interpret end position")
+            continue
+        rospy.loginfo("Moving end effector to x -> {}, y -> {}, z -> {}".format(x, y, z))
+        pose_goal.position.x = 0.0 + x
+        pose_goal.position.y = -0.7 + y
+        pose_goal.position.z = -0.2 + z
+
+        # set pose target
+        group.set_pose_target(pose_goal)
+
+        if group.go(wait=True):
+            rospy.loginfo("Path planning was a success")
+        else:
+            rospy.logwarn("Path planning was a failure")
+
+        # Calling `stop()` ensures that there is no residual movement
+        group.stop()
+        
+        # It is always good to clear your targets after planning with poses.
+        # Note: there is no equivalent function for clear_joint_value_targets()
+        group.clear_pose_targets()
+
+        rospy.sleep(0.1) # sleep to give other threads processing time
 
 if __name__ == "__main__":
     main()
