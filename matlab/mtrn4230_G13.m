13da1ec016436abea8db43008eed0e11f26f8066
jamiemccoll1 <jamiemccoll1@gmail.com>
2020-08-07 15:51:47 +1000

    Had global x and y mixed around
    had global x and y mixed around

--- matlab/mtrn4230_G13.m
+++ matlab/mtrn4230_G13.m
@@ -72,8 +72,8 @@ function P3D = toCamera(px, py, d)
 end
 
 function P3D = toGlobal(cam_p3d)
-    P3D.x = cam_p3d.y;
-    P3D.y = -cam_p3d.x;
+    P3D.x = -cam_p3d.x;
+    P3D.y = cam_p3d.y;
     P3D.z = 2.0 - cam_p3d.z;
 end
 
