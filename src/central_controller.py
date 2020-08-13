890304c8469fc25998cda695466f68f3fad0157a
James McColl <jamiemccoll1@gmail.com>
2020-08-13 14:16:44 +1000

    central controller now with orders

--- src/central_controller.py
+++ src/central_controller.py
@@ -16,11 +16,12 @@ from moveit_commander.conversions import pose_to_list
 
 
 object_list = []
+order_input = {}
 static_order_plan = []
 
 def vision_callback(string):
     global object_list
-    rospy.loginfo("Vision callback triggered")
+    #rospy.loginfo("Vision callback triggered")
     lines = string.data.split('|')
     msg_id = lines[0]
     elms = lines[1:-1]
@@ -34,8 +35,26 @@ def vision_callback(string):
         tup = (e[1], float(e[2]), float(e[3]), float(e[4]))
         object_list[i] = tup
 
+def order_callback(string):
+    global order_input, static_order_plan
+    if static_order_plan:
+        return
+    rospy.loginfo("Order callback triggered: "+string.data)
+    lines = string.data.split('|')
+    msg_id = lines[0]
+    elms = lines[1:]
+
+    order_input = {}
+
+    for i, elm in enumerate(elms):
+        if not elm:
+            break
+        e = elm.split(',')
+        order_input[e[0]] = int(e[1])
+    #rospy.loginfo(" order callback: " +str(order_input))
+
 def main():
-    global object_list, static_order_plan
+    global object_list, static_order_plan, order_input
     moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node('motion_planner', anonymous=True)
                     
@@ -58,6 +77,8 @@ def main():
     pose_goal.orientation.w = 1.0  # hard coded orientation, to be figured out
 
     rospy.Subscriber("cv_pos", String, vision_callback)
+    rospy.Subscriber("cv_order", String, order_callback)
+
 
     drop_point = ("drop_pt", 0.6, 0.7, 0.3)
 
@@ -82,10 +103,9 @@ def main():
             continue
 
         rospy.loginfo("Object list: "+str(object_list))
+        rospy.loginfo("Order list: "+str(order_input))
         # Get order input
         if not static_order_plan:
-
-            order_input = {"red": 1, "blue": 1, "yellow": 1}
 
             # Construct high-level path/order := L
 
@@ -98,17 +118,17 @@ def main():
                     if num_required == 0:
                         break
                     if elm[0] == key:
-                        static_order_plan.append(elm)
-                        static_order_plan.append(drop_point)
+                        static_order_plan.append((key,elm))
+                        static_order_plan.append(("dropoff",drop_point))
                         num_required -= 1
             # End TODO
 
         for target in static_order_plan:
-            x = target[1]
-            y = target[2]
-            z = target[3]
+            x = target[1][1]
+            y = target[1][2]
+            z = target[1][3]+0.2
 
-            rospy.loginfo("Moving end effector to x -> {}, y -> {}, z -> {}".format(x, y, z))
+            rospy.loginfo("Moving end effector to {}: x -> {}, y -> {}, z -> {}+0.2".format(target[0], x, y, z-0.2))
             pose_goal.position.x = 0.0 + x
             pose_goal.position.y = -0.7 + y
             pose_goal.position.z = -0.2 + z
