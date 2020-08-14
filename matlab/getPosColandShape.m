4007a98298d6296009304c73904536ed927e07e1
jamiemccoll1 <jamiemccoll1@gmail.com>
2020-08-14 12:33:44 +1000

    Added getPosColandShape to GUI

--- matlab/getPosColandShape.m
+++ matlab/getPosColandShape.m
@@ -1,7 +1,7 @@
 function [blockstats,xPos,yPos] = getPosColandShape(image, colour, shape)
     % colour: red=1, blue=2, yellow=3
-    % shape: rectangle = 1, circle = 2, triangle = 3
-    blocks = imread(image);
+    % shape: rectangle = 1, circle = 2, triangle = 3, any = 4
+    blocks = image;%imread(image);
     
     conveyerMask = getConveyerMask(blocks);
     
@@ -11,54 +11,35 @@ function [blockstats,xPos,yPos] = getPosColandShape(image, colour, shape)
         channel1 = [255.000,255.000];
         channel2 = [0.000,5.000];
         channel3 = [0.000,5.000];
-        if shape == 1
-            area_min = 550;
-            area_max = 600;
-        end
-        if shape == 2
-            area_min = 300;
-            area_max = 350;
-        end
-        if shape == 3
-            area_min = 125;
-            area_max = 175;
-        end
     end
     if colour == 2
         % Thresholds for channel blue colour
         channel1 = [0.000,5.000];
         channel2 = [0.000,5.000];
-        channel3 = [255.000,255.000];
-        if shape == 1
-            area_min = 550;
-            area_max = 600;
-        end
-        if shape == 2
-            area_min = 300;
-            area_max = 350;
-        end
-        if shape == 3
-            area_min = 125;
-            area_max = 175;
-        end
+        channel3 = [255.000,255.000];       
     end
     if colour == 3
         % Thresholds for channel blue colour
         channel1 = [255.000,255.000];
         channel2 = [255.000,255.000];
         channel3 = [0.000,5.000];
-        if shape == 1
-            area_min = 525;
-            area_max = 600;
-        end
-        if shape == 2
-            area_min = 300;
-            area_max = 350;
-        end
-        if shape == 3
-            area_min = 125;
-            area_max = 175;
-        end
+    end
+    
+    if shape == 1
+        area_min = 525;
+        area_max = 600;
+    end
+    if shape == 2
+        area_min = 300;
+        area_max = 350;
+    end
+    if shape == 3
+        area_min = 125;
+        area_max = 175;
+    end
+    if shape == 4
+        area_min = 50;
+        area_max = 70000;
     end
     
     BW = (I(:,:,1) >= channel1(1) ) & (I(:,:,1) <= channel1(2)) & ...
@@ -66,8 +47,8 @@ function [blockstats,xPos,yPos] = getPosColandShape(image, colour, shape)
             (I(:,:,3) >= channel3(1) ) & (I(:,:,3) <= channel3(2));
     % remove objects not on conveyer
     BW = BW & conveyerMask;
-    figure(1);
-    imshow(BW)
+    %figure(1);
+    %imshow(BW)
     stats = regionprops(BW, 'BoundingBox','Centroid','area');
     blockstats = regionprops('table', BW, 'all');
     accepted = blockstats.Area >= area_min & blockstats.Area <= area_max;
@@ -76,19 +57,24 @@ function [blockstats,xPos,yPos] = getPosColandShape(image, colour, shape)
     row = 1;
     for i = find(accepted).'
         if centroids(i,1) ~= 0
-            xPos(row,1) = centroids(i,1);
-            yPos(row,1) = centroids(i,2);
+            xPos(row,1) = round(centroids(i,1));
+            yPos(row,1) = round(centroids(i,2));
             row = row + 1;
         else
-            break
+            break;
         end
     end
-    figure(2);
-    imshow(BW);
-    hold on
-    axis on
-    plot(xPos,yPos,'b*');
-    hold off
+    
+    if row == 1
+        xPos = [-1];
+        yPos = [-1];
+    end
+%     figure(2);
+%     imshow(BW);
+%     hold on
+%     axis on
+%     plot(xPos,yPos,'b*');
+%     hold off
 end
 
 function mask = getConveyerMask(image)
