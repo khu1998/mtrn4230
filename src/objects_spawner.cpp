2901616bfa04332138ccbd14f95e66a328cd78cf
Kent Hu <khu.1998@icloud.com>
2020-08-23 16:09:57 +1000

    src/objects_spawner.cpp: now able to clear table of objects or delete spawned objects on table

--- src/objects_spawner.cpp
+++ src/objects_spawner.cpp
@@ -13,83 +13,111 @@
 #include <boost/uuid/uuid_generators.hpp> // generators
 #include <boost/uuid/uuid_io.hpp>         // streaming operators etc.
 #include <gazebo_msgs/ApplyBodyWrench.h>
-#include <gazebo_msgs/SetModelState.h>
+#include <gazebo_msgs/DeleteModel.h>
+#include <gazebo_msgs/ModelStates.h>
 #include <gazebo_msgs/SpawnModel.h>
 #include <ros/ros.h>
-#include <std_msgs/Int8MultiArray.h>
-#include <urdf/model.h>
+#include <ros/callback_queue.h>
 
 #include <array>
+#include <cstdio>
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <random>
 #include <sstream>
 #include <string>
 #include <vector>
+#include <unordered_set>
 
-std::vector<std::string> get_spawn_objects_xml(const std::vector<std::string> &colours, const std::vector<std::string> &shapes, const ros::NodeHandle& nh);
+#include <termios.h>
+#include <unistd.h>
 
-int main(int argc, char **argv)
-{
+using name = std::string;
+std::unordered_set<name> names; // object names, only valid ones are kept, i.e. must be within bounds of spawn location
+
+static std::vector<std::string>
+get_spawn_objects_xml(const std::vector<std::string> &colours,
+                      const std::vector<std::string> &shapes,
+                      const ros::NodeHandle &nh);
+static void
+update_objects(const gazebo_msgs::ModelStates &current_model_states);
+static int getch();
+static bool within_table_bounds(int x, int y, int z);
+
+int main(int argc, char **argv) {
   unsigned int number_of_picks;
   int delay;
   std::vector<std::string> colours;
   std::vector<std::string> shapes;
   boost::program_options::options_description desc("Options");
-  desc.add_options()
-    ("help,h", "produce help message")
-    ("delay,d", boost::program_options::value(&delay)->default_value(10), "delay between object spawn. if delay is less than 0, user is prompted to press ENTER before object is spawned")
-    ("colours,c", boost::program_options::value(&colours)->multitoken()->default_value(std::vector<std::string>{"red", "blue", "yellow"}, "red, blue, yellow"), "colours to use to spawn objects. available colours: blue red yellow, if not specified defaults to all")
-    ("number-of-picks,n", boost::program_options::value(&number_of_picks)->default_value(10), "number of objects to be picked up by the robot")
-    ("shapes,s", boost::program_options::value(&shapes)->multitoken()->default_value(std::vector<std::string>{"box", "cylinder", "triangle"}, "box, cylinder, triangle"), "shapes to use to spawn objects. available shapes: box cylinder triangle, if not specified, defaults to all")
-    ("random,r", "object spawn is random otherwise fixed location in the centre of the table")
-  ;
+  desc.add_options()("help,h", "produce help message")(
+      "delay,d", boost::program_options::value(&delay)->default_value(10),
+      "delay between object spawn. if delay is less than 0, user is prompted "
+      "to press ENTER before object is spawned")(
+      "colours,c",
+      boost::program_options::value(&colours)->multitoken()->default_value(
+          std::vector<std::string>{"red", "blue", "yellow"},
+          "red, blue, yellow"),
+      "colours to use to spawn objects. available colours: blue red yellow, if "
+      "not specified defaults to all")(
+      "number-of-picks,n",
+      boost::program_options::value(&number_of_picks)->default_value(10),
+      "number of objects to be picked up by the robot")(
+      "shapes,s",
+      boost::program_options::value(&shapes)->multitoken()->default_value(
+          std::vector<std::string>{"box", "cylinder", "triangle"},
+          "box, cylinder, triangle"),
+      "shapes to use to spawn objects. available shapes: box cylinder "
+      "triangle, if not specified, defaults to all")(
+      "random,r", "object spawn is random otherwise fixed location in the "
+                  "centre of the table");
 
   boost::program_options::variables_map vm;
-  boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+  boost::program_options::store(
+      boost::program_options::parse_command_line(argc, argv, desc), vm);
   boost::program_options::notify(vm);
 
-  if (vm.count("help"))
-  {
+  if (vm.count("help")) {
     std::cout << desc << '\n';
     return 0;
   }
-
   const bool &random = vm.count("random");
 
   ros::init(argc, argv, "objects_spawner");
   ros::NodeHandle nh;
-  srand(time(0));
-  // service client for service /gazebo/spawn_urdf_model
   ros::ServiceClient spawn_client =
       nh.serviceClient<gazebo_msgs::SpawnModel>("/gazebo/spawn_urdf_model");
+  ros::ServiceClient delete_client =
+      nh.serviceClient<gazebo_msgs::DeleteModel>("/gazebo/delete_model");
+  ros::ServiceClient wrench_client =
+      nh.serviceClient<gazebo_msgs::ApplyBodyWrench>(
+          "/gazebo/apply_body_wrench");
   gazebo_msgs::SpawnModel::Request spawn_model_req;
   gazebo_msgs::SpawnModel::Response spawn_model_resp;
+  gazebo_msgs::DeleteModel::Request delete_model_req;
+  gazebo_msgs::DeleteModel::Response delete_model_resp;
+  gazebo_msgs::ApplyBodyWrench::Request apply_wrench_req;
+  gazebo_msgs::ApplyBodyWrench::Response apply_wrench_resp;
 
-  // publisher for current_blocks
-  // ros::Publisher current_blocks_publisher =
-  //     nh.advertise<std_msgs::Int8MultiArray>("current_blocks", 1);
-  // std_msgs::Int8MultiArray current_blocks_msg; 
-  // current_blocks_msg.data.clear();
-
-  // make sure /gazebo/spawn_urdf_model service is service_ready
-  bool service_ready = false;
-  while (!service_ready)
-  {
-    service_ready = ros::service::exists("/gazebo/spawn_urdf_model", true);
-    ROS_INFO("waiting for spawn_urdf_model service");
-    ros::Duration(0.5).sleep();
+  // make sure spawn model and delete model service is ready
+  if (!ros::service::waitForService("/gazebo/spawn_urdf_model")) {
+    ROS_ERROR("check for service /gazebo/spawn_urdf_model");
+    return 1;
   }
-  ROS_INFO("spawn_urdf_model service is ready");
+  ROS_INFO("/gazebo/spawn_urdf_model service is ready");
 
-  service_ready = false;
-  while (!service_ready)
-  {
-    service_ready = ros::service::exists("/gazebo/set_model_state", true);
-    ROS_INFO("waiting for set_model_state service");
-    ros::Duration(0.5).sleep();
+  if (!ros::service::waitForService("/gazebo/delete_model")) {
+    ROS_ERROR("check for service /gazebo/delete_model");
+    return 1;
   }
-  ROS_INFO("set_model_state service is ready");
+  ROS_INFO("/gazebo/delete_model service is ready");
+
+  if (!ros::service::waitForService("/gazebo/apply_body_wrench")) {
+    ROS_ERROR("check for service /gazebo/apply_body_wrench");
+    return 1;
+  }
+  ROS_INFO("/gazebo/apply_body_wrench service is ready");
 
   // prepare the spawn model service message
   spawn_model_req.initial_pose.position.x = 0.0;
@@ -102,80 +130,177 @@ int main(int argc, char **argv)
   spawn_model_req.reference_frame = "world";
   spawn_model_req.robot_namespace = "spawn objects";
 
+  // prepare conveyor belt service
+  apply_wrench_req.wrench.force.x = 0.0;
+  apply_wrench_req.wrench.force.y = -1.0;
+  apply_wrench_req.wrench.force.z = 0.0;
+  apply_wrench_req.duration = ros::Duration(0.25);
+  apply_wrench_req.reference_frame = "world";
+
+  auto clear_table = [&]() -> void {
+    for (const auto& name : names) {
+      apply_wrench_req.body_name = name + "::base_link";
+      apply_wrench_req.start_time = ros::Time::now();
+      if (!wrench_client.call(apply_wrench_req, apply_wrench_resp)) {
+        ROS_ERROR("failed to connect with gazebo server");
+        std::exit(1);
+      }
+      if (!apply_wrench_resp.success) {
+        ROS_ERROR("failed to clear table");
+        std::exit(1);
+      }
+    }
+    names.clear();
+  };
+
+  auto delete_objects = [&]() -> void {
+    for (const auto& name : names) {
+      delete_model_req.model_name = name;
+      if (!delete_client.call(delete_model_req, delete_model_resp)) {
+        ROS_ERROR("failed to connect with gazebo server");
+        std::exit(1);
+      }
+      if (!delete_model_resp.success) {
+        ROS_ERROR("failed to clear table");
+        std::exit(1);
+      }
+    }
+    names.clear();
+  };
+
+  ros::NodeHandle model_handler;
+  ros::CallbackQueue model_state_queue;
+  model_handler.setCallbackQueue(&model_state_queue);
+  ros::Subscriber model_states_subscriber = model_handler.subscribe("/gazebo/model_states", 1, update_objects);
+  ros::AsyncSpinner spinner(1, &model_state_queue);
+  spinner.start();
+
   // generate random numbers between 0 and 0.5 for spawning of blocks
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_real_distribution<> xdis(-0.45, 0.45);
   std::uniform_real_distribution<> ydis(0.2, 0.45);
-  const auto& spawn_objects = get_spawn_objects_xml(colours, shapes, nh);
-  while (ros::ok())
-  {
-    for (const auto &spawn_object : spawn_objects)
-    {
+  const auto &spawn_objects = get_spawn_objects_xml(colours, shapes, nh);
+  while (ros::ok()) {
+    for (const auto &spawn_object : spawn_objects) {
+      if (delay < 0) {
+        ROS_INFO_STREAM(
+            "Press <ENTER> to spawn a new object, <d> to delete all objects, "
+            "<c> to clear objects off table, or <q> to stop");
+        switch (::getch()) {
+        case 10:
+          ROS_INFO_STREAM("Spawning new object...");
+          break;
+        case 99:
+          ROS_INFO_STREAM("Clearing objects off table...");
+          clear_table();
+          ROS_INFO_STREAM("Finished clearing objects off table");
+          continue;
+          break;
+        case 100:
+          ROS_INFO_STREAM("Deleting all objects on table...");
+          delete_objects();
+          ROS_INFO_STREAM("Finished deleting objects off table");
+          continue;
+          break;
+        case 113:
+          ROS_INFO_STREAM("Quitting...");
+          return 0;
+          break;
+        default:
+          ROS_INFO_STREAM("Please press one of <ENTER>, <d>, <c>, or <q>");
+          continue;
+          break;
+        }
+      }
+
       // initialize model_name
-      spawn_model_req.model_name = "spawn object " + boost::lexical_cast<std::string>(boost::uuids::random_generator()());
+      spawn_model_req.model_name = "object-" + boost::lexical_cast<std::string>(boost::uuids::random_generator()());
       spawn_model_req.model_xml = spawn_object;
-      if (random)
-      {
+      if (random) {
         spawn_model_req.initial_pose.position.x = xdis(gen);
         spawn_model_req.initial_pose.position.y = ydis(gen);
       }
 
-      if (!spawn_client.call(spawn_model_req, spawn_model_resp))
-      {
+      if (!spawn_client.call(spawn_model_req, spawn_model_resp)) {
         ROS_ERROR("failed to connect with gazebo server");
         return 1;
       }
-      if (!spawn_model_resp.success)
-      {
+      if (!spawn_model_resp.success) {
         ROS_ERROR("failed to spawn model");
         return 1;
       }
-
-      // publish current cylinder blocks status, all cylinder blocks will be
-      // published no matter if it's successfully spawned, or successfully
-      // initialized in speed
-      // current_blocks_publisher.publish(current_blocks_msg); // not sure if required
-
-      // frequency control, spawn one cylinder in each loop; delay
-      // time decides density of the cylinders
+      
       ros::spinOnce();
-      if (delay >= 0)
-      {
+
+      if (delay >= 0) {
         ros::Duration(delay).sleep();
-      }
-      else
-      {
-        std::cout << "Press ENTER to spawn a new object";
-        std::cin.get();
       }
     }
   }
   return 0;
 }
 
-std::vector<std::string> get_spawn_objects_xml(const std::vector<std::string> &colours, const std::vector<std::string> &shapes, const ros::NodeHandle& nh)
-{
-  auto file_content_to_string = [&](const std::string &path) {
-    std::ifstream ifs(&path[0]);
-    std::stringstream ss;
-    ss << ifs.rdbuf();
-    return ss.str();
+static std::vector<std::string>
+get_spawn_objects_xml(const std::vector<std::string> &colours,
+                      const std::vector<std::string> &shapes,
+                      const ros::NodeHandle &nh) {
+  auto read_file = [&](const std::string &path) -> std::string {
+    auto stream = std::ifstream(path, std::ios::in | std::ios::ate);
+    auto file_size = stream.tellg();
+    stream.seekg(0);
+    std::string contents(file_size, 0);
+    stream.read(&contents[0], contents.size());
+    return contents;
   };
   std::vector<std::string> spawn_objects_xml;
   std::string path;
-  for (const auto &colour : colours)
-  {
-    for (const auto &shape : shapes)
-    {
-      if (!nh.getParam(boost::str(boost::format("/%1%_%2%_path") % colour % shape), path))
-      {
-        ROS_WARN_STREAM("failed to get path to urdf for " << colour << ' ' << shape);
+  for (const auto &colour : colours) {
+    for (const auto &shape : shapes) {
+      if (!nh.getParam(
+              boost::str(boost::format("/%1%_%2%_path") % colour % shape),
+              path)) {
+        ROS_WARN_STREAM("failed to get path to urdf for " << colour << ' '
+                                                          << shape);
         continue;
       }
-      ROS_INFO_STREAM("path to urdf for " << colour << ' ' << shape << " file found");
-      spawn_objects_xml.emplace_back(file_content_to_string(path));
+      ROS_INFO_STREAM("path to urdf for " << colour << ' ' << shape
+                                          << " file found");
+      spawn_objects_xml.emplace_back(read_file(path));
     }
   }
   return spawn_objects_xml;
+}
+
+static void
+update_objects(const gazebo_msgs::ModelStates &current_model_states) {
+  for (std::size_t i = 0; i < current_model_states.name.size(); ++i) {
+    if (names.find(current_model_states.name[i]) != names.end()) {
+      continue;
+    }
+    int x = current_model_states.pose[i].position.x;
+    int y = current_model_states.pose[i].position.y;
+    int z = current_model_states.pose[i].position.z;
+    if (current_model_states.name[i].find("object-") != 0 || within_table_bounds(x, y, z)) {
+      continue;
+    }
+    names.insert(current_model_states.name[i]);
+  }
+}
+
+static int getch() {
+  static struct termios oldt, newt;
+  tcgetattr(STDIN_FILENO, &oldt); // save old settings
+  newt = oldt;
+  newt.c_lflag &= ~ICANON & ~ECHO;         // disable buffering and echo of character in terminal
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt); // apply new settings
+  int c = getchar(); // read character (non-blocking)
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // restore old settings
+  return c;
+}
+
+static bool within_table_bounds(int x, int y, int z) {
+  return (-0.5 <= x && x <= 0.5) &&
+         (-0.5 <= y && y <= 0.5) &&
+         (0.19 <= z && z <= 0.21);
 }
