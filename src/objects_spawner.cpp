638e7881f48a4422287951b36efa91f5c921b479
Kent Hu <khu.1998@icloud.com>
2020-08-25 20:36:11 +1000

    objects_spawner: added /clear_table node that calls clear_table function

--- src/objects_spawner.cpp
+++ src/objects_spawner.cpp
@@ -13,6 +13,7 @@
 #include <gazebo_msgs/SpawnModel.h>
 #include <ros/callback_queue.h>
 #include <ros/ros.h>
+#include <std_msgs/Bool.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -154,6 +155,12 @@ int main(int argc, char **argv) {
     }
     names.clear();
   };
+  auto clear_table_callback = [&](const std_msgs::Bool::ConstPtr& msg) -> void {
+    if (msg->data) {
+      clear_table();
+    }
+  }
+  ros::Subscriber model_states_subscriber = nh.subscribe("/clear_table", 1, clear_table_callback);
 
   auto delete_objects = [&]() -> void {
     for (auto name : names) {
